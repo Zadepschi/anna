11d10c040d39cc910e75215a054ef564b44edb7e
Natalia <zadepschi@yahoo.com>
2023-07-10 11:12:14 +0300

    animation changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 gsap.from(".title", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 1})
-gsap.from(".paragraph", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 1.5})
-gsap.from(".yellow", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 2})
-gsap.from(".icon", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 2.5})
+gsap.from(".paragraph", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 1.3})
+gsap.from(".yellow", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 1.6})
+gsap.from(".icon", {y: -20,ease: "back", opacity: 0,  duration: 1, delay: 1.9})
